7b2161885c429c20ab8934b466f54dd8ef3a5b23
Altamish Mahomed <gambit318@gmail.com>
2026-02-26 16:14:10 +0200

    Try fixing site interactions.

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,32 @@
 // js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import
-  {
-    getFirestore,
-    doc,
-    setDoc,
-    getDoc,
-    updateDoc,
-    onSnapshot,
-    serverTimestamp,
-    collection,
-    getDocs
-  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+{
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  onSnapshot,
+  serverTimestamp,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import { firebaseConfig } from "./firebase-config.js";
 
 
 
-const app = initializeApp(firebaseConfig);
+let app;
+try
+{
+  app = initializeApp(firebaseConfig);
+  console.log("Firebase initialized successfully");
+} catch (error)
+{
+  console.error("Error initializing Firebase:", error);
+}
+
 export const db = getFirestore(app);
 
 export
