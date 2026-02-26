233545d82243201c1d46b9a81fa2df82ec67c1ef
Altamish Mahomed <gambit318@gmail.com>
2026-02-26 15:32:03 +0200

    Try deploy to Firebase.

---
+++ js/firebase-config.template.js
@@ -0,0 +1,11 @@
+// js/firebase-config.template.js
+// Duplicate this file as firebase-config.js and fill in your values.
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
--- js/firebase.js
+++ js/firebase.js
@@ -1,33 +1,27 @@
 // js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  onSnapshot,
-  serverTimestamp,
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import
+  {
+    getFirestore,
+    doc,
+    setDoc,
+    getDoc,
+    updateDoc,
+    onSnapshot,
+    serverTimestamp,
+    collection,
+    getDocs
+  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA6sA_c3yNUZvvo_dZanhydLn7jXl-55hU",
-  authDomain: "punto-8888.firebaseapp.com",
-  projectId: "punto-8888",
-  storageBucket: "punto-8888.firebasestorage.app",
-  messagingSenderId: "362458130631",
-  appId: "1:362458130631:web:3236b4c130549566d576e2",
-  measurementId: "G-671HVQXCR4"
-};
+import { firebaseConfig } from "./firebase-config.js";
 
 
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-export {
+export
+{
   doc,
   setDoc,
   getDoc,
