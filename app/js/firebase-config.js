700bd8759f01b3d6e3e8cf3a27ba6a0c05aa57a5
Altamish Mahomed <gambit318@gmail.com>
2026-04-09 11:56:43 +0200

    Moved app to own subdomain and added Under Construction page.

---
+++ app/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6sA_c3yNUZvvo_dZanhydLn7jXl-55hU",
+    authDomain: "punto-8888.firebaseapp.com",
+    projectId: "punto-8888",
+    storageBucket: "punto-8888.firebasestorage.app",
+    messagingSenderId: "362458130631",
+    appId: "1:362458130631:web:3236b4c130549566d576e2",
+    measurementId: "G-671HVQXCR4"
+};
