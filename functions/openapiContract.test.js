cd9cbed1c03029836ace35c7831979b9c569f9a2
Altamish Mahomed <gambit318@gmail.com>
2026-09-25 16:17:17 +0200

    test: use CommonJS loading for contract validators

--- functions/openapiContract.test.js
+++ functions/openapiContract.test.js
@@ -320,15 +320,9 @@ async function loadContract() {
         "utf8",
       );
 
-      const [
-        { validate: validateOpenApi },
-        { validate: validateResponse },
-        { parse },
-      ] = await Promise.all([
-        import("@scalar/openapi-validator"),
-        import("@scalar/json-schema-validator"),
-        import("yaml"),
-      ]);
+      const { validate: validateOpenApi } = require("@scalar/openapi-validator");
+      const { validate: validateResponse } = require("@scalar/json-schema-validator");
+      const { parse } = require("yaml");
 
       const document = parse(openapiSource);
       const openapiResult = await validateOpenApi(document);
