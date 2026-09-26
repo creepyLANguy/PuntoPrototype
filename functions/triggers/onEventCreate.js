4a7b48fcc1765a3a8cec5781dabd10e0425cca81
Altamish Mahomed <gambit318@gmail.com>
2026-09-26 13:35:13 +0200

    refactor(functions): reuse shared region in Firestore trigger

--- functions/triggers/onEventCreate.js
+++ functions/triggers/onEventCreate.js
@@ -1,10 +1,11 @@
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 const { processScoreEvent } = require("../services/scoreService");
+const { REGION } = require("../services/constants");
 
 exports.onEventCreate = onDocumentCreated(
   {
     document: "courts/{courtId}/events/{eventId}",
-    region: "africa-south1",
+    region: REGION,
     retry: true,
   },
   processScoreEvent,
