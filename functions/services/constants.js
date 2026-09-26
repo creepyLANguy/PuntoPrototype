cae26329178324ccd3ad1f9ee8d88b5883faf8b9
Altamish Mahomed <gambit318@gmail.com>
2026-09-26 13:34:20 +0200

    chore(regions): align Functions and docs to africa-south1

--- functions/services/constants.js
+++ functions/services/constants.js
@@ -2,12 +2,10 @@ const SCORE_CHECKPOINTS_COLLECTION = "scoreCheckpoints";
 const DEFAULT_TEAM_NAMES = { A: "Team A", B: "Team B" };
 const DEFAULT_PLAYER_NAMES = { A1: "", A2: "", B1: "", B2: "" };
 const REGION = "africa-south1";
-const HOSTING_REWRITE_REGION = "europe-west1";
 module.exports = {
   SCORE_CHECKPOINTS_COLLECTION,
   DEFAULT_TEAM_NAMES,
   DEFAULT_PLAYER_NAMES,
   REGION,
-  HOSTING_REWRITE_REGION,
 };
 // Formatting verified by CI.
